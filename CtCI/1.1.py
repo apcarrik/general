4eb08a90c315131a42a0033466e00f484dad53e0
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-04 16:09:47 -0400

    implementing linked list for hashmap

--- CtCI/1.1.py
+++ CtCI/1.1.py
@@ -1,25 +1,51 @@
 '''
 Solution for Problem 1.1 in Cracking the Coding Interview.
-brute_force solves the problem with n^2 runtime, where n is the length of the input string
-
+brute_force solves the problem with O(n^2) runtime, where n is the length of the input string
+hash_method solves the problem with O(n) runtime using a hashmap with a linked list
 '''
+# Class LnkdListNode
+class LnkdListNode:
+    def __init__(self, ch):
+        self.data = ch
+        self.next = None
+
+# Class LnkdList
+class Lnkdlist:
+    def __init__(self):
+        self.head = None
+
+    def add(self, ch):
+        newnode = LnkdListNode(ch)
+        if (self.head):
+            nextnode = self.head
+            while nextnode.next:
+                nextnode = nextnode.next
+            nextnode.next = newnode
+        else:
+            self.head = newnode
+    
+    def contains(self, ch):
+        if (self.head):
+            querynode = self.head
+            while (querynode):
+                if(querynode.data == ch): return True
+                querynode = querynode.next
+        return False
 
 # Class CharHash 
 class CharHash:
   def __init__(self, strlen):
       self.length = strlen
-      self.chartable = [[]] * self.length # TODO: Turn this into a real linked list
+      self.chartable = [Lnkdlist] * self.length
 
   def hlookup(self,ch):
       return hash(ch)%self.length
 
   def add(self, ch):
-      self.chartable[self.hlookup(ch)].append(ch)
+      self.chartable[self.hlookup(ch)].add(ch)
 
   def contains(self, ch):
-      for chmatch in self.chartable[self.hlookup(ch)] :
-        if (ch == chmatch): return True
-      return False
+      self.chartable[self.hlookup(ch)].contains(ch)
 
 # Function: brute_force 
 def brute_force(str):
