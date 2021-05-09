a4f0ecccef9228576006ae32d83cc1c6459702fd
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-08 23:22:53 -0400

    adding boilerplate code for problem 4.4

---
+++ CtCI/4.4.py
@@ -0,0 +1,10 @@
+'''
+Solution for problem 4.4 in Cracking the Coding Interview
+checkBalanced takes an input binary tree and checks if it is balanced.
+'''
+#TODO: decide on data structure (adjacency list or matrix) for Binary tree
+
+def checkBalanced(bt):
+    #TODO: decide on approach for checking if balanced
+    #TODO: implement
+    return False # Or True
