ebe839fc2226bcaec55efa30f7f4ef79d3b87a02
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-10 12:04:21 -0400

    adding queue data structs

--- CtCI/4.4.py
+++ CtCI/4.4.py
@@ -3,11 +3,39 @@ Solution for problem 4.4 in Cracking the Coding Interview
 checkBalanced takes an input binary tree and checks if it is balanced.
 '''
 #Using Adjacency list representation of binary tree
+class qnode:
+    def __init__(self , data):
+        self.data = data
+        self.next = None
+
+class queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def enqueue(self, data):
+        newnode = qnode(data)
+        if(self.head == None and self.tail == None): #Queue empty
+            self.head = newnode
+            self.tail = newnode
+        else:
+            oldtail = self.tail
+            oldtail.nexty = qnode(data)
+            self.tail = newnode
+    def dequeue(self):
+        if(self.head == None and self.tail == None): #Queue empty
+            return None #TODO change this to throw error
+        else:
+            oldhead = self.head
+            self.head = oldhead.next
+            if(self.head == None):
+                self.tail = None
+            return oldhead.data
+    def isempty(self):
+        return True if(self.head == None and self.tail == None) else False
 
 
-
-def checkBalanced(bt):
-    # Idea: perform BFS starting at root node, and keep track of longest and shortest path
+def checkBalanced(bt, root):
+    # Idea: perform BFS starting at root node, and keep track of longest and shortest path (high/low water marks)
     #TODO: implement
     return False # Or True
 
@@ -19,5 +47,5 @@ def makebt(balanced):
         return [[2,3],[1,4,5],[1],[2,6],[2],[4]]
 
 
-assert checkBalanced(makebt(True)) == True
-assert checkBalanced(makebt(False)) == False
+assert checkBalanced(makebt(True),1) == True
+assert checkBalanced(makebt(False),1) == False
