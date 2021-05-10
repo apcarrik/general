62a220fdd209595b20f4ed6f2e1c8135aca980c6
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-09 22:39:49 -0400

    adding function to make binary tree and assert statements to test checkBalanced

--- CtCI/4.4.py
+++ CtCI/4.4.py
@@ -2,10 +2,22 @@
 Solution for problem 4.4 in Cracking the Coding Interview
 checkBalanced takes an input binary tree and checks if it is balanced.
 '''
-#TODO: decide on data structure (adjacency list or matrix) for Binary tree
+#Using Adjacency list representation of binary tree
+
+
 
 def checkBalanced(bt):
-    #TODO: decide on approach for checking if balanced
-    # Idea: could I do random walks to check size of path? would the probabilities be enough?
+    # Idea: perform BFS starting at root node, and keep track of longest and shortest path
     #TODO: implement
     return False # Or True
+
+def makebt(balanced):
+    # creates a binary tree, either balanced or not
+    if(balanced):
+        return [[2,3],[1,4,5],[1,6,7],[2,8],[2],[3,9],[3],[4],[6]]
+    else:
+        return [[2,3],[1,4,5],[1],[2,6],[2],[4]]
+
+
+assert checkBalanced(makebt(True)) == True
+assert checkBalanced(makebt(False)) == False
