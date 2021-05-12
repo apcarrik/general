fc2d2a0af10fe5624db2f55d6d843c0c0a2af71d
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-12 16:53:55 -0400

    revising return value on error and adding failure testcase

--- CtCI/5.2.py
+++ CtCI/5.2.py
@@ -2,14 +2,14 @@
 Solution for problem 5.2 in Cracking the Coding
 
 binaryToString takes a float value that is between 0 and 1, and returns the 32-bit binary fraction of the value
-if one exists. If it cannot be represented as a 32-bit binary fraction, it prints "ERROR" and returns NaN.
+if one exists. If it cannot be represented as a 32-bit binary fraction, it prints and returns the string "ERROR".
 '''
 
 def binaryToString(re):
     # test if 32 bit precision is not enough for input
     if( re % (2**(-32)) != 0.0):
         print("ERROR")
-        return float("NaN")
+        return "ERROR"
     # get each binary digit of input
     retstr = ""
     for d in range(1,33):
@@ -21,6 +21,10 @@ def binaryToString(re):
     return retstr
 
 # Test function
-testinput = 2**(-15) + 2**(-23) + 2**(-26) + 2**(-31) + 2**(-32)
-expectedoutput = "00000000000000100000001001000011"
-assert binaryToString(testinput) == expectedoutput
+testinputpass = 2**(-15) + 2**(-23) + 2**(-26) + 2**(-31) + 2**(-32)
+expectedoutputpass = "00000000000000100000001001000011"
+assert binaryToString(testinputpass) == expectedoutputpass
+
+testinputfail = 2**(-15) + 2**(-23) + 2**(-26) + 2**(-31) + 2**(-33)
+expectedoutputfail = "ERROR"
+assert binaryToString(testinputfail) == expectedoutputfail
