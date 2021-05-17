958f751fa69780dda417e52389f95c94f2cb2ed1
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-16 23:33:35 -0400

    adding a little more to magicIndex

--- CtCI/8.3.py
+++ CtCI/8.3.py
@@ -2,10 +2,18 @@
 Solution for problem 8.3 in Cracking the Coding Interview
 '''
 
-# magicIndex TODO: fill out function description
-def magicIndex():
-    # TODO: Implement magic index
-    return True
+# magicIndex TODO: finish filling out function description
+def magicIndex(A):
+    # test if fn is strictly increasing or strictly decreasing
+    n = len(A)
+    if (A[0] < A[n - 1]): # fn is strictly increasing
+        # TODO: check if fn could intersect A[i] = i line
+
+    else: # fn is strictly decreasing
+        #TODO: finish this
+
+
 
 # TODO: implement test for magicIndex
-assert magicIndex() == True
+A = [-3,-2,-1,4,5,7,9]
+assert magicIndex(A) == True
