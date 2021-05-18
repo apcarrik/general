99679e3b45ad329b752e9f1dca69e930b2a6d3db
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-17 21:58:58 -0400

    filling out function, need to review & debug

--- CtCI/8.3.py
+++ CtCI/8.3.py
@@ -2,15 +2,55 @@
 Solution for problem 8.3 in Cracking the Coding Interview
 '''
 
-# magicIndex TODO: finish filling out function description
+# magicIndex TODO: debug function
 def magicIndex(A):
     # test if fn is strictly increasing or strictly decreasing
     n = len(A)
     if (A[0] < A[n - 1]): # fn is strictly increasing
-        # TODO: check if fn could intersect A[i] = i line
-
+        if(A[0] <= 0 and A[n-1] >= n-1):
+            leftidx = 0
+            rightidx = n-1
+            while rightidx-leftidx >=1:
+                if(leftidx == A[leftidx] or rightidx == A[rightidx]):
+                    return True
+                difference = rightidx - leftidx
+                if (difference == 1):
+                    break
+                elif(difference %2 == 0): # even distance
+                    midpoint = leftidx + (difference / 2)
+                    if(midpoint <= A[midpoint]):
+                        leftidx = midpoint
+                    else:
+                        rightidx = midpoint
+                else: # odd distance
+                    midpoint = leftidx + (difference / 2)
+                    if(midpoint >= A[midpoint]):
+                        rightidx = midpoint
+                    else:
+                        leftidx = midpoint+1
     else: # fn is strictly decreasing
-        #TODO: finish this
+        #TODO: debug this
+        if(A[0]>=0 and A[n-1] <= n-1):
+            leftidx = 0
+            rightidx = n-1
+            while rightidx-leftidx >=1:
+                if(leftidx != A[leftidx] or rightidx != A[rightidx]):
+                    return True
+                difference = rightidx - leftidx
+                if(difference == 1):
+                    break
+                elif(difference %2 == 0): # even distance
+                    midpoint = leftidx + (difference / 2)
+                    if(midpoint <= A[midpoint]):
+                        leftidx = midpoint
+                    else:
+                        rightidx = midpoint
+                else: # odd distance
+                    midpoint = leftidx + (difference / 2)
+                    if(midpoint >= A[midpoint]):
+                        rightidx = midpoint
+                    else:
+                        leftidx = midpoint+1
 
 
 
