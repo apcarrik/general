e3cbdc5e9a14859900037194334f56daf115863e
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-05-23 22:40:14 -0400

    creating code skeleton

---
+++ CtCI/9.2.py
@@ -0,0 +1,14 @@
+'''
+Solution for problem 9.2 in Cracking the Coding Interview
+
+'''
+
+# TODO: decide what data structure to use for people & social network
+
+# shortestPath finds the least number of people between p1 and p2 in a given social network net
+def shortestPath(net, p1, p2):
+    # TODO: Implement
+
+
+# Testing shortestPath
+# TODO: create tests for shortestPath
