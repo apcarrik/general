9accd39653a0610fd8d22a6cb2a1c9423f0aed28
Adam Carriker <apcarrik@ncsu.edu>
2022-02-12 18:07:24 -0500

    adding queue to baselibs

---
+++ CtCI/baselibs/queue/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='queue',
+    version='0.1',
+    description='Holds implementation of simple queue.',
+    url='http://',
+    author='Adam Carriker',
+    author_email='apcarrik@ncsu.edu',
+    license='GPL',
+    packages=['queue'],
+    zip_safe=False,
+)
