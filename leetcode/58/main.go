f1427494647bcecbbb4c23cf435152d03b447c89
Adam Carriker <apcarrik@ncsu.edu>
2023-01-21 18:37:58 -0800

    Leetcode #58 - Found a much shorter way

--- leetcode/58/main.go
+++ leetcode/58/main.go
@@ -1,21 +1,5 @@
+import "strings"
 func lengthOfLastWord(s string) int {
-    // Idea: starting from end, go character by character searching for last word
-    // - Step 1: search for non-space character
-    // - Step 2: start counter
-    // - Step 3: end counter when you encounter space character or beginning of string
-    wordlen := 0
-    for i := len(s)-1; i >= 0; i-- {
-        if wordlen == 0 {
-            if s[i] != ' ' {
-                wordlen += 1
-            }
-        } else {
-            if s[i] == ' ' {
-                return wordlen
-            } else {
-                wordlen += 1
-            }
-        }
-    }
-    return wordlen
+    s2 := strings.TrimSpace(s)
+    return len(s2) - strings.LastIndexByte(s2,' ') - 1
 }
