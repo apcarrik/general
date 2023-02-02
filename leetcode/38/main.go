befbeb5c96c6dde18958a4eac4e6dade3dd1e6da
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-02-02 14:33:23 -0800

    Adding working go solution to leetcode problem 38

--- leetcode/38/main.go
+++ leetcode/38/main.go
@@ -1,3 +1,28 @@
+import (
+    "strings"
+    "strconv"
+)
 func countAndSay(n int) string {
-    
+    // Idea: start from 1 and iterate until you hit n
+    saying := "1"
+    for i:=1; i<n; i++{
+        // Iterate through each char in saying
+        var prevChar byte
+        prevCharCount := 0
+        var newSaying string
+        for j:=0; j<len(saying); j++ {
+            if saying[j] == prevChar {
+                prevCharCount += 1
+            } else {
+                if prevCharCount > 0 {
+                    newSaying += strconv.Itoa(prevCharCount) + string(rune(prevChar))
+                }
+                prevChar = saying[j]
+                prevCharCount = 1
+            }
+        }        
+        newSaying += strconv.Itoa(prevCharCount) + string(rune(prevChar))
+        saying = newSaying
+    }
+    return saying
 }
