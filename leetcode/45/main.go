8a00a9edf7d461b1352c38cbad9d854a16666c2a
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-02-08 22:00:27 -0800

    Fixed memory issue with queue implementation but now functionality is not working

--- leetcode/45/main.go
+++ leetcode/45/main.go
@@ -1,34 +1,70 @@
+import "fmt"
+type QNode struct {
+    Value int
+    Next *QNode
+}
+
 func jump(nums []int) int {
     // Idea: Consider nums an encoding of a graph. Use Dijkstra's algorithm (or similar BFS-based algorithm) to find shortest path between nodes of first element to last element of nums. Starting at the first.
-    // BFS search uses a Priority Queue to keep nodes in order to search in correct order
+    // BFS search uses a Queue to keep nodes in order to search in correct order
     n := len(nums)
 
-    // Keep slice denoting shortest path to each node from node 0.
+    // Keep slice denoting shortest path to each node from node 0 (indicies correspond to nums)
     shortestPath := make([]int, n)
     // initialize shortest path of all nodes to +inf
-    for i:=1;i<n;i++{ // first node has path of 0
+    shortestPath[0] = 0 // first node has path of 0
+    for i:=1;i<n;i++{ 
         shortestPath[i] = n+1
     }
 
-
-    // I'm going to use a slice to implement Priority Queue
+    
     // two queues: nodeQ indicates the index of node in nums, pathQ indicates current path of node on nodeQ
-    nodeQ := []int{0}
-    pathQ := []int{0}
-    for w:= 0; w<n*n; w++ { // max # of iterations is n^2
-        if len(nodeQ) == 0 {
+    sentinel := QNode{-1,nil}
+    nodeQHead := QNode{0,&sentinel}
+    nodeQTail := nodeQHead
+    pathQHead := QNode{0,&sentinel}
+    pathQTail := pathQHead
+    
+    for true { // Warning: max # of iterations is n!, but I couldn't get for loop to bound that high
+        if nodeQHead == sentinel {
             break
         }
-
         // Pop priority from nodeQ and pathQ
-        thisNode := nums[nodeQ[0]]
-        nodeQ := nodeQ[1:]
-        thisPathLen := pathQ[0]
-        pathQ := pathQ[1:]
+        thisNodeIdx := nodeQHead.Value
+        nodeQHead = *nodeQHead.Next
+        thisPathLen := pathQHead.Value
+        pathQHead = *pathQHead.Next
 
-        // TODO: implement Dijkstra's algorithm
-
-
+        // Check if
+        if thisPathLen <= shortestPath[thisNodeIdx] { 
+            shortestPath[thisNodeIdx] = thisPathLen
+            if thisNodeIdx < (n-1) {
+                // Add all nodes we can reach from this one to queue
+                for i:=1; i<=nums[thisNodeIdx]; i++{
+                    if thisNodeIdx+i<n {
+                        newNode := QNode{thisNodeIdx+i,&sentinel}
+                        if nodeQHead == sentinel {
+                            nodeQHead = newNode
+                            nodeQTail = nodeQHead
+                        } else {
+                            nodeQTail.Next = &newNode
+                            nodeQTail = newNode
+                        }
+                        // nodeQ = append(nodeQ, thisNodeIdx+i)
+                        newNode = QNode{thisPathLen+1,&sentinel}
+                        if pathQHead == sentinel {
+                            pathQHead = newNode
+                            pathQTail = pathQHead
+                        } else {
+                            pathQTail.Next = &newNode
+                            pathQTail = newNode
+                        }
+                        // pathQ = append(pathQ, thisPathLen+1)
+                    }
+                }
+            }
+        }
     }
+
     return shortestPath[n-1]
 }
