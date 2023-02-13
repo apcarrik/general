d91c21704bc26fcc3a80233d02315f2189a1f98d
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-02-13 10:21:08 -0800

    Made more progress but still not quite working.
    I think something is wrong with my linked list of PathNodes. It always has a maximum of 2 paths in the end, even though it splits many more than 2 times. Also, it places too few paths on finishedPaths, but it is more than 2 so there's probably two bugs here.

--- leetcode/2556/main.go
+++ leetcode/2556/main.go
@@ -1,24 +1,25 @@
 import (
-    "strconv"
-    //"fmt"
+    "fmt"
 )
 
-type PathNode struct {
+type Cell struct {
     X int
     Y int
-    Next *PathNode
+    Usage int
 }
 
-// type QNode struct {
-//     N *PathNode
-//     Next *QNode
-// }
+type PathNode struct {
+    Path []*Cell
+    Next *PathNode
+    Prev *PathNode
+}
 
-func validCoordinates(x int, y int, m int, n int) bool {
-    if x>=0 && x<m && y>=0 && y<n {
-        return true
+func shallowCopyPath(oldPath []*Cell) []*Cell{
+    newPath := []*Cell{}
+    for i:=0; i<len(oldPath); i++{
+        newPath = append(newPath,oldPath[i])
     }
-    return false
+    return newPath
 }
 
 func isPossibleToCutPath(grid [][]int) bool {
@@ -31,84 +32,124 @@ func isPossibleToCutPath(grid [][]int) bool {
 
     m := len(grid)
     n := len(grid[0])
-    pathSentinel := &PathNode{X:-1, Y:-1, Next:nil}
-    paths := []*PathNode{&PathNode{X:0, Y:0, Next:pathSentinel}} 
+    
+    // create map of 1 cells indexed by coordinates
+    cellMap := map[int]map[int]*Cell{}
+    for x:=0; x<m; x++{
+        for y:=0; y<n; y++{
+            if grid[x][y] == 1 {
+                _, exists := cellMap[x]
+                if !exists {
+                    cellMap[x] = map[int]*Cell{}
+                }
+                    cellMap[x][y] = &Cell{X:x,Y:y}
+            }
+        }
+    }
+    fmt.Println("m:",m,"n:",n,"cellMap",cellMap)
+    // create path data structures
+    // pathSentinel := &PathNode{Path: nil, Next:nil, Prev:nil}
+    pathsHead := &PathNode{Path: []*Cell{cellMap[0][0]}, Next: nil, Prev:nil}
+    var finishedPathsHead *PathNode
 
     //edge case: m & n is 1
     // if m==1 && n==1 {
     //     return false
     // }
+    numSplits := 0
+    for i:=0; i<(m+n)*2; i++{
+        // Iterate to next cell in path, or split path if both options avaliable
+        fmt.Println("i:",i,"pathsHead",pathsHead)
 
-    for i:=0; i<m+n; i++{
-
-        // Check if all paths have finished
-        allPathsFinished := true
-        for j:=0; j<len(paths); j++{
-            if paths[j].X != m-1 || paths[j].Y != n-1 {
-                allPathsFinished = false
-            }
-        }
-        if allPathsFinished {
+        if pathsHead == nil {
+            fmt.Println("All paths finished, moving on.")
             break
         }
-
-        // Iterate to next cell in path, or split path if both options avaliable
-        for j:=0; j<len(paths); j++{
-            //fmt.Println("j:",j,"paths[j]:",paths[j])
-            currX := paths[j].X
-            currY := paths[j].Y
-            if validCoordinates(currX+1,currY,m,n) && grid[currX+1][currY] == 1 && validCoordinates(currX,currY+1,m,n) && grid[currX][currY+1] == 1 {
-                // split path
-                //fmt.Println("splitting path")
-                newPathNode1 := &PathNode{X:currX+1 , Y:currY , Next: paths[j]}
-                newPathNode2 := &PathNode{X:currX , Y:currY+1 , Next: paths[j]}
-                paths[j] = newPathNode1
-                paths = append(paths,newPathNode2)
-            } else if validCoordinates(currX+1,currY,m,n) && grid[currX+1][currY] == 1 {
-                // move down
-                //fmt.Println("moving down")
-                newPathNode := &PathNode{X:currX+1 , Y:currY , Next: paths[j]}
-                paths[j] = newPathNode
-            } else if validCoordinates(currX,currY+1,m,n) && grid[currX][currY+1] == 1 {
-                // move right
-                //fmt.Println("moving right")
-                newPathNode := &PathNode{X:currX , Y:currY+1 , Next: paths[j]}
-                paths[j] = newPathNode
-            } else if !(paths[j].X == m-1 && paths[j].Y==n-1) {
-                // path ended before finish cell; remove from paths\
-                //fmt.Println("removing path ", paths[j])
-                if j == 0 {
-                    paths = paths[1:]
-                } else if j == len(paths)-1{
-                    paths = paths[:len(paths)-1]
+        pathsCtr :=0
+        for pathIter:=pathsHead; pathIter != nil; pathIter = pathIter.Next{
+            pathsCtr += 1
+            lastCell := pathIter.Path[len(pathIter.Path)-1]
+            fmt.Println("pathIter",pathIter,"lastCell:",lastCell.X,",",lastCell.Y)
+            if lastCell == cellMap[m-1][n-1]{
+                // path ended on finish cell; place path in finishedPaths and remove from paths
+                fmt.Println("placing path on finishedPaths")
+                if pathIter == pathsHead {
+                    pathsHead = pathsHead.Next
+                    if pathIter.Next != nil {                        
+                        pathIter.Next.Prev = pathsHead
+                    }
+                } else if pathIter.Next == nil {
+                    pathIter.Prev.Next = pathIter.Next
                 } else {
-                    paths = append(paths[:j], paths[j+1:]...)
+                    pathIter.Prev.Next = pathIter.Next
+                    pathIter.Next.Prev = pathIter.Prev
+                }
+
+                pathIter.Prev = nil
+                pathIter.Next = nil
+                if finishedPathsHead == nil {
+                    finishedPathsHead = pathIter
+                }  else {
+                    finishedPathsHead.Next = pathIter
+                    pathIter.Prev = finishedPathsHead
+                }
+            } else {
+                _, onesCellRightExists := cellMap[lastCell.X][lastCell.Y+1]
+                _, onesCellDownExists := cellMap[lastCell.X+1][lastCell.Y]
+
+                if onesCellRightExists && onesCellDownExists {
+                    // split path
+                    fmt.Println("splitting path")
+                    numSplits++
+                    prevPathNode := pathIter.Prev
+                    newPath := shallowCopyPath(pathIter.Path)
+                    newPath = append(newPath,cellMap[lastCell.X][lastCell.Y+1])
+                    newPathNode := &PathNode{Path: newPath, Next: pathIter, Prev:prevPathNode}
+                    pathIter.Path = append(pathIter.Path,cellMap[lastCell.X+1][lastCell.Y])     
+                    pathIter.Prev = newPathNode      
+                    if pathIter == pathsHead {
+                        pathsHead = pathIter.Prev
+                    }
+                    fmt.Println(" pathIter.Prev",pathIter.Prev," pathIter.Prev.X,Y:",pathIter.Prev.Path[0].X, pathIter.Prev.Path[0].Y)
+                    fmt.Println(" pathIter",pathIter," pathIter.Prev.X,Y:",pathIter.Prev.Path[0].X, pathIter.Prev.Path[0].Y)         
+                } else if onesCellRightExists {
+                    // move right
+                    fmt.Println("moving right")
+                    pathIter.Path = append(pathIter.Path,cellMap[lastCell.X][lastCell.Y+1])
+                } else if onesCellDownExists {
+                    // move down
+                    fmt.Println("moving down")
+                    pathIter.Path = append(pathIter.Path,cellMap[lastCell.X+1][lastCell.Y])
+                } else {
+                    // path ended before finish cell; remove path from paths
+                    fmt.Println("removing path ", pathIter)
+                    if pathIter.Next == nil && pathIter == pathsHead {
+                        pathsHead = pathsHead.Next
+                    } else if pathIter == pathsHead {
+                        pathsHead = pathsHead.Next
+                        pathIter.Next.Prev = pathsHead
+                    } else if pathIter.Next == nil {
+                        pathIter.Prev.Next = pathIter.Next
+                    } else {
+                        pathIter.Prev.Next = pathIter.Next
+                        pathIter.Next.Prev = pathIter.Prev
+                    }
                 }
             }
         }
+        fmt.Println("pathsCtr:",pathsCtr)
 
     }
-    //fmt.Println(paths)
-    // Check how many paths exists
-    switch len(paths) {
-        case 0:
+    fmt.Println("numSplits:",numSplits)
+    fmt.Println("-----\npathsHead",pathsHead)
+    // Check how many paths exists 
+    fmt.Println("finishedPathsHead",finishedPathsHead)
+    switch  {
+        case finishedPathsHead == nil:
             return true
-        case 1:
+        case finishedPathsHead.Next == nil:
             // check that there is at least 1 cell that is not beginning or end
-            nonEndCellFound := false
-            for paths[0] != pathSentinel{
-                //fmt.Println(paths[0],"m:",m,"n:",n)
-                if paths[0].X ==0 && paths[0].Y == 0 {
-                    nonEndCellFound = nonEndCellFound
-                } else if paths[0].X == m-1 && paths[0].Y ==n-1{
-                    nonEndCellFound = nonEndCellFound
-                } else {
-                    //fmt.Println("nonEndCellFound")
-                    nonEndCellFound = true
-                }
-                paths[0] = paths[0].Next
-            }
-            if nonEndCellFound {
+            if len(finishedPathsHead.Path) > 2  {
                 return true
             } else{
                 return false
@@ -116,25 +157,28 @@ func isPossibleToCutPath(grid [][]int) bool {
     }
     // More than 1 path. Need to see if there is critical point that every path uses
     // Make freq map of cells on paths. check if any cell is on every path, if so return true, else false
-    var freqMap = make(map[string]int)
-    for j:=0; j<len(paths); j++{
-        for paths[j] != pathSentinel {
-            coordStr := "X" + strconv.Itoa(paths[j].X) + "Y" + strconv.Itoa(paths[j].Y)
-            _, exists := freqMap[coordStr]
-            if exists {
-                freqMap[coordStr] += 1
-            } else {
-                freqMap[coordStr] = 1
-            }
-            paths[j] = paths[j].Next
-        }
-    }
-    //fmt.Println(freqMap)
 
-    for k,v := range freqMap {
-        if  v == len(paths) && k != "X-1Y-1" && k != "X0Y0" && k != "X"+strconv.Itoa(m-1)+"Y"+strconv.Itoa(n-1) {
-            //fmt.Println("returning true; k=", k, "v=",v)
-            return true
+
+    numPaths := 0
+    for pathIter:=finishedPathsHead; pathIter != nil; pathIter = pathIter.Next{
+        fmt.Println("pathIter",pathIter)
+        numPaths += 1
+        for i:=0; i< len(pathIter.Path); i++{
+            pathIter.Path[i].Usage += 1
+        }
+    
+    }
+    fmt.Println("numPaths",numPaths)
+    // Drop beginning/end path nodes
+    delete(cellMap[0],0)
+    delete(cellMap[m-1],n-1)
+
+    for _,nArr := range cellMap {
+        for _,cell := range nArr {
+            fmt.Println("cell",cell)
+            if cell.Usage == numPaths{ // if a 1 cell is used by all paths, it is critical cell and can disconnect matrix by being flipped
+                return true
+            }
         }
     }
     return false
