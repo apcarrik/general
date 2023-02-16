7cafe1b910c650a08a290dfd71e522713639ec50
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-02-16 14:33:04 -0800

    Updating with cleaner recursive solution

--- leetcode/104/main.go
+++ leetcode/104/main.go
@@ -8,40 +8,19 @@
  */
 
 func maxDepth(root *TreeNode) int {
+    // Idea: recusivley check the depth of all children. Return 1+ the greater of the two depths
     if root == nil {
         return 0
     }
-
-    // Idea: BFS
-    // uses priority queues to keep track of unexplored nodes and their depths
-    var unexplored_nodes []*TreeNode
-    var unexplored_node_depths []int
-
-    unexplored_nodes = append(unexplored_nodes, root)
-    unexplored_node_depths = append(unexplored_node_depths, 1)
-    greatest_depth := 0
-
-    for len(unexplored_nodes) > 0 {
-        node := unexplored_nodes[0]
-        unexplored_nodes = unexplored_nodes[1:]
-        node_depth := unexplored_node_depths[0]
-        unexplored_node_depths = unexplored_node_depths[1:]
-
-        if node_depth > greatest_depth {
-            greatest_depth = node_depth
-        }
-
-        if node.Left != nil {
-            unexplored_nodes = append(unexplored_nodes,node.Left)
-            unexplored_node_depths = append(unexplored_node_depths,node_depth+1)
-        }
-
-        if node.Right != nil {
-            unexplored_nodes = append(unexplored_nodes,node.Right)
-            unexplored_node_depths = append(unexplored_node_depths,node_depth+1)
-        }
+    leftDepth, rightDepth := 0, 0
+    if root.Left != nil {
+        leftDepth = maxDepth(root.Left)
     }
-
-    return greatest_depth
-    
+    if root.Right != nil {
+        rightDepth = maxDepth(root.Right)
+    }
+    if leftDepth > rightDepth {
+        return 1+leftDepth
+    }
+    return 1+rightDepth
 }
