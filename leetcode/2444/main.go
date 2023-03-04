371f651e83f32cd5925953e5b696aa2d062c446e
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-03-04 14:37:50 -0800

    Adding go solution to leetcode problem 2444
    Runtime 2037 ms (Beats 20%), Memory 9.8 MB (Beats 20%).
    Not the most efficient, but it works. I've left my development code in here for previous approaches that didn't work very well.

---
+++ leetcode/2444/main.go
@@ -0,0 +1,136 @@
+/*
+   Notes:   - what if we found the "pools" of fixed-bound subarrays? i.e. largest fixed-bound subarrays in nums
+            - we know that all subarrays under that would count, and there will be TriangularNumber(len(subarray))
+            of them, where TriangularNumber(n) = (n^2+n)/2 (!!!ONLY if maxK = minK!!!)
+*/
+// import "fmt"
+func triangularNumber(n int64) int64 {
+    return (n*n+n)/2
+}
+
+func countSubarraysSameMinMax(nums []int, minK int) int64 {
+    // Idea: iterate through each element of nums, until you find subarray with min==minK. Note the starting index, and keep iterating until you reach an element that is != minK. Then, append the triangular number of the length of this subarray and start over at the next element.
+
+    fixedBoundCount := int64(0)
+    startIdx := -1
+    for i,num := range nums {
+        if num == minK {
+            // within bounds, keep track
+            if startIdx == -1 {
+                startIdx = i
+            }
+        } else {
+            // out of bounds, add to fixedBoundCount if fixedBound found
+            // fmt.Println(i, "startIdx",startIdx)
+
+            if startIdx > -1  {
+                fixedBoundCount += triangularNumber(int64(i-startIdx))
+                // fmt.Println("fixedBoundCount",fixedBoundCount)
+            }
+            startIdx = -1
+        }
+    }
+    if startIdx > -1  {
+        fixedBoundCount += triangularNumber(int64(len(nums) - startIdx))
+        // fmt.Println("fixedBoundCount",fixedBoundCount)
+    }
+    return fixedBoundCount
+}
+
+/*
+   Brute-force solution
+*/
+func countSubarrays(nums []int, minK int, maxK int) int64 {
+    // Idea: check all subarrays, keeping track of max/min for each and which are fixed-bound
+    // - To do this, iterate through each index of nums as start of array, and iterate through remaining indicies of nums as end of array. This allows you to keep track of min/max for each start index efficiently.
+    if minK == maxK {
+        return countSubarraysSameMinMax(nums, minK)
+    } else {
+        fixedBound := 0
+        for i:=0; i<len(nums); i++{
+            max := 0
+            min := 1000001
+            jLoop:
+            for j:=i; j<len(nums); j++{
+                if nums[j] > max {
+                    max = nums[j]
+                } 
+                if nums[j] < min {
+                    min = nums[j]
+                }
+                if min < minK || max > maxK {
+                    break jLoop
+                }
+                if min == minK && max == maxK {
+                    fixedBound++
+                }
+            }
+        }
+        return int64(fixedBound) 
+    }
+}
+
+/*
+   Notes:   - the smallest fixed-bound subarray will have minimum and maximum as ends
+            - maybe we could try to find these subarrays with min/max at ends that are fixed-bound, and then grow 
+            them from there
+            - Idea: find all indicies of minK and maxK. For each index of minK, search through all indicies of maxK 
+            until you find a fixed-bound subarray between them. Then, try to grow subarray in each direction until 
+            it no longer satifsies fixed-bound subarray, or you hit another minK or maxK.
+*/
+// import "fmt"
+
+// func countSubarrays(nums []int, minK int, maxK int) int64 {
+//     n := len(nums)
+
+//     // Find indicies for all instances of minK in nums
+//     minKIndicies := []int{}
+//     for i:=0; i<n; i++{
+//         if nums[i] == minK {
+//             minKIndicies = append(minKIndicies,i)
+//         }
+//     }
+//     fmt.Println(minKIndicies)
+
+//     // for each minK, search left then right for fixed-bound subarrays with minK & maxK as extreemes, until you hit
+//     // another minK/maxK, keeping track of fixed-bound subarrays you encounter
+//     fixedBoundCount := 0
+//     for _,minKIdx := range minKIndicies {
+//         max := 0
+//         min := 1000001
+
+//         leftLoop:
+//         for i:=minKIdx-1; i>=0; i-- {
+//             if nums[i] == minKIdx || min < minK || max < maxK {
+//                 break leftLoop
+//             }
+//             if nums[i] > max {
+//                 max = nums[i]
+//             } 
+//             if nums[i] < min {
+//                 min = nums[i]
+//             }
+//             if min == minK && max == maxK {
+//                 fixedBoundCount++
+//             }            
+//         }
+
+//         rightLoop:
+//         for i:=minKIdx+1; i<n; i++ {
+//             if nums[i] == minKIdx || min < minK || max < maxK {
+//                 break rightLoop
+//             }
+//             if nums[i] > max {
+//                 max = nums[i]
+//             } 
+//             if nums[i] < min {
+//                 min = nums[i]
+//             }
+//             if min == minK && max == maxK {
+//                 fixedBoundCount++
+//             }            
+//         }
+
+//     }
+//     return int64(fixedBoundCount) 
+// }
