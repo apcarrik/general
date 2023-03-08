87cb4c140a5c36d0049f90c52e6cad45ba172d67
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-03-07 22:26:25 -0800

    Cleaning up code
    Runtime
    49 ms
    Beats
    49.52%
    Memory
    7.3 MB
    Beats
    8.65%

--- leetcode/875/main.go
+++ leetcode/875/main.go
@@ -1,11 +1,3 @@
-/*Notes:
-    - Binary search problem
-    - max k: maximum pile of bananas
-    - min k: 1
-    - note: low may be +1 or -1 of actual answer, or next highest stack. Hence edge cases check at end.
-
-*/
-
 func getHours(pilesPtr *[]int, k int) int {
     piles := *pilesPtr
     hours := 0
@@ -19,18 +11,8 @@ func getHours(pilesPtr *[]int, k int) int {
 }
 
 func minEatingSpeed(piles []int, h int) int {
-
-    // find max pile for high
-    maxPile := 0
-    for _,pile := range piles {
-        if pile > maxPile {
-            maxPile = pile
-        }
-    }
-
-    // binary search
     low := 1
-    high := maxPile
+    high := 1000000001
     for high > low {
         mid := (high-low)/2 + low
         hours := getHours(&piles, mid)
@@ -39,29 +21,9 @@ func minEatingSpeed(piles []int, h int) int {
         } else {
             high = mid - 1
         }
-    }
-
-    // edge cases
-    if low == maxPile {
-        secondMaxPile := 0
-        for _,pile := range piles {
-            if pile > secondMaxPile && pile < maxPile {
-                secondMaxPile = pile
-            }
-        }
-        if secondMaxPile > 0 && getHours(&piles, secondMaxPile) <= h {
-            return secondMaxPile
-        }
-    }
-    
-    if low-1 > 0 && getHours(&piles, low-1) <= h {
-        return low-1
-    } 
-    
+    }    
     if getHours(&piles, low) > h {
         return low+1
-    }
-    
-    return low
-    
+    }    
+    return low    
 }
