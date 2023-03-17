28eaa9008b98f90094c0a450b4a47b105fa61652
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-03-17 10:27:35 -0700

    Cleaning up code a little

--- leetcode/1964/main.go
+++ leetcode/1964/main.go
@@ -1,13 +1,12 @@
 /* Notes:
     - for each element of obstacles, we want the number of obstacles before this one that are greater than or equal to all obstacles before it.
     - I think we can start from beginning and compute obstacle course length for each element
-    - We can use pre-computed obstacle course lengths to save time, by searching one index to the left of current until you hit one that is less than or equal to current and summing that with current
+    - We can use pre-computed obstacle course lengths to save time, a form of memoization
 */
 
-/* Problem: this won't scale well. Worst case every element is strictly decreasing, it will be a pyramid sum, so O(n^2) runtime
-    - Idea: keep a map with obstacle lengths as keys, and array of indicies for that obstacle length as value, with larger indicies at the front.
+/* Solution: Iterate through each index (height) of obstacles from beginning to end. Keep track of the longest obstacle course at each height (longest), and keep a lookup table (longestLookup) with {key: obstacle course length, value: smallest height to have this obstacle course length} and a separate slice with the keys from this lookup table (longestLookupKeys) that is kept in increasing order. 
 
-    Idea: keep rolling map with key = longest values and value = smallest height in obstacle course. You'll need to keep sorting the keys each iteration of for loop around obstacles. In each iteration, check each longest value if the max height < this height, else move to the next one
+For each height in obstacles, find if any previous obstacle course lengths have a minimum height less than or equal to this height, and if so use that length + 1 as the length of this obstacle course. Add this new obstacle course length to longest, and update longestLookup and longestLookupKeys as appropriate.
 
 */
 
@@ -38,41 +37,43 @@ func binarySearch(x int, thingsPtr *[]int) int { // gaurentees to return index o
 
 
 func longestObstacleCourseAtEachPosition(obstacles []int) []int {
-    // iterate through obstacles, recording the longest obstacle course using obstacleLookup
     longest := []int{}
-    longestLookup := map [int]int{} // key: longeset obstacle course, value: smallest height to have this obstacle course // OLD: value: largest height in obstacle course
+    longestLookup := map [int]int{} // key: obstacle course length, value: smallest height to have this obstacle course
     longestLookupKeys := []int{} // ordered list of keys from longest lookup (smallest to largest)
+
+    // iterate through obstacles, recording the longest obstacle course using obstacleLookup
     for _, height := range obstacles {
 
         // find largest previous obstacle course
-        prevSum := 0
+        prevLength := 0
         for i:=len(longestLookupKeys) - 1; i>=0; i-- {
             k := longestLookupKeys[i]
             if longestLookup[k] <= height {
-                prevSum = k
+                prevLength = k
                 break                
             }
         }
         // update longest
-        newSum := prevSum +1
-        longest = append(longest, newSum)
+        newLength := prevLength +1
+        longest = append(longest, newLength)
 
-        if _,ok := longestLookup[newSum]; ok {
-            if longestLookup[newSum] > height {
-                longestLookup[newSum] = height
+        // update longestLookup
+        if _,ok := longestLookup[newLength]; ok {
+            if longestLookup[newLength] > height {
+                longestLookup[newLength] = height
             }
         } else {
-            longestLookup[newSum] = height
-            // add newSum to longest lookup keys
-            newPosition := binarySearch(newSum, &longestLookupKeys)
+            longestLookup[newLength] = height
+            // add newLength to longestLookupKeys
+            newPosition := binarySearch(newLength, &longestLookupKeys) // keep longestLookupKeys in increasing order
             if newPosition == -1 {
-                longestLookupKeys = append([]int{newSum}, longestLookupKeys...)
+                longestLookupKeys = append([]int{newLength}, longestLookupKeys...)
             } else if newPosition < len(longestLookupKeys) - 1{
                 pre := longestLookupKeys[:newPosition+1]
                 post := longestLookupKeys[newPosition+1:]
-                longestLookupKeys = append(append(pre, newSum), post...)
+                longestLookupKeys = append(append(pre, newLength), post...)
             } else {
-                longestLookupKeys = append(longestLookupKeys, newSum)
+                longestLookupKeys = append(longestLookupKeys, newLength)
             }
         }
     }
