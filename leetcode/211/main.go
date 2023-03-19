331195574ea872c6d7f850612c6f4fc396298485
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-03-19 16:22:14 -0700

    Came up with this nifty bitmask check for words that unfortunately made the runtime worse
    Runtime
    1645 ms
    Beats
    17.90%
    Memory
    9.5 MB
    Beats
    100%

--- leetcode/211/main.go
+++ leetcode/211/main.go
@@ -3,26 +3,67 @@
     - It seems like knowing the length of all words is important here
     - Since only 10k max words, it is probably better to store all words than use Trie (3 dots would open up to 17k searches)
     - We could store words in map with key: length, value: word as string
-    - What if we created a 26 bit match array for each word, corresponding to if each letter is used?
 
-ex. word: "applejack" -> bitmap[1,0,1...] -> a, c, e, j, k, l, p
-    match: "apple" -> bitmap[1,0,0,...] -> a, e, l, p
+    Optimizations:
+    - 
+
+
+    - What if we created a 26 bit mask array for each word, corresponding to if each letter is used?
+    - Note, we would need it to be a partial match, as match could have wildcard '.' characters
+
+ex. word:  "apple" -> bitmap[1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,...] -> a, e, l, p
+    match: "app.e" -> bitmap[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,...] -> a, e, p
+    Operator: XOR  -> bitmap[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,...] -> word XOR match
+    Operator: AND  -> bitmap[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,...] -> match AND (word XOR match)
 */
 
 type WordDictionary struct {
     WordMap map [int][]string    
+    BitMask map [int][]int32 // 26 bit int
 }
 
 
 func Constructor() WordDictionary {
-    return WordDictionary{map [int][]string{}}
+    return WordDictionary{
+        WordMap: map [int][]string{},
+        BitMask: map[int][]int32{},
+    }
+}
+
+func createBitmask(word string) int32 {
+    bitmask := int32(0)
+    for i:=0; i<len(word); i++{
+        char := word[i]
+        if char != '.'{
+            bitmask = bitmask | 1<<(char-97)
+        }
+    }
+    return bitmask
+}
+
+func checkBitmask(word, match int32) bool {
+    // fmt.Println(word, match, (word ^ match), (word ^ match), match & (word ^ match))
+    if match & (word ^ match) == 0 {
+        return true
+    }
+    return false
 }
 
 
 func (this *WordDictionary) AddWord(word string)  {
+    // fmt.Println(checkBitmask(createBitmask("apple"), createBitmask("appre")))
+    // fmt.Println(checkBitmask(createBitmask("apple"), createBitmask("apple")))
+    // fmt.Println(checkBitmask(createBitmask("apple"), createBitmask("app.e")))
+    // return
     wordLen := len(word)
+    newBitmask := createBitmask(word)
     if _,ok := this.WordMap[wordLen]; ok {
-        for _,existingWord := range this.WordMap[wordLen] {
+        for j,existingWord := range this.WordMap[wordLen] {
+            // Optimization: check character bitmask first
+            if !checkBitmask(this.BitMask[wordLen][j], newBitmask) {
+                continue
+            }
+            
             sameWord := true
             for i:=0; i<len(word); i++{
                 if !(word[i] == '.') && word[i] != existingWord[i]{
@@ -35,8 +76,10 @@ func (this *WordDictionary) AddWord(word string)  {
             }
         }
         this.WordMap[wordLen] = append(this.WordMap[wordLen], word)
+        this.BitMask[wordLen] = append(this.BitMask[wordLen], newBitmask)
     } else {
         this.WordMap[wordLen] = []string{word}
+        this.BitMask[wordLen] = append(this.BitMask[wordLen], newBitmask)
     }
     return
 }
@@ -45,7 +88,13 @@ func (this *WordDictionary) AddWord(word string)  {
 func (this *WordDictionary) Search(word string) bool {
     wordLen := len(word)
     
-    for _,existingWord := range this.WordMap[wordLen] {
+    matchBitmask := createBitmask(word)
+    for j,existingWord := range this.WordMap[wordLen] {
+        // Optimization: check character bitmask first
+        if !checkBitmask(this.BitMask[wordLen][j], matchBitmask) {
+            continue
+        }
+
         sameWord := true
         for i:=0; i<len(word); i++{
             if !(word[i] == '.') && word[i] != existingWord[i]{
