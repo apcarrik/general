c7ad0b9310df1c30109dffc4a3f70b88a1b5c70b
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-03-27 11:02:02 -0700

    Removing extra grid copy and just modifying grid itself
    Runtime
    4 ms
    Beats
    90.16%
    Memory
    3.9 MB
    Beats
    82.51%

--- leetcode/64/main.go
+++ leetcode/64/main.go
@@ -7,36 +7,27 @@ func minPathSum(grid [][]int) int {
 	m := len(grid) 
 	n := len(grid[0])
 
-	//initialize mpsGrid
-	mpsGrid := [][]int{}
-	for ri,row := range grid {
-		mpsGrid = append(mpsGrid, []int{})
-		for j:=0; j<len(row); j++ {
-			mpsGrid[ri] = append(mpsGrid[ri], 0)
-		}
-	}
-
-	// fill out mpsGrid one row at a time and one column at a time
+	// fill out grid one row at a time and one column at a time
 	for ri, row:= range grid {
 		for ci, _:= range row {			
 			if ri==0 && ci == 0 {
-				mpsGrid[ri][ci] = grid[ri][ci]
+				grid[ri][ci] = grid[ri][ci]
 			} else if ri == 0 {
-				mpsGrid[ri][ci] = grid[ri][ci] + mpsGrid[ri][ci-1]
+				grid[ri][ci] = grid[ri][ci] + grid[ri][ci-1]
 			} else if ci == 0 {
-				mpsGrid[ri][ci] = grid[ri][ci] + mpsGrid[ri-1][ci]
+				grid[ri][ci] = grid[ri][ci] + grid[ri-1][ci]
 			} else {
-				top := mpsGrid[ri-1][ci]
-				left := mpsGrid[ri][ci-1]
+				top := grid[ri-1][ci]
+				left := grid[ri][ci-1]
 				if top < left {
-					mpsGrid[ri][ci] = grid[ri][ci] + top
+					grid[ri][ci] = grid[ri][ci] + top
 				} else {
-					mpsGrid[ri][ci] = grid[ri][ci] + left
+					grid[ri][ci] = grid[ri][ci] + left
 				}
 			}
 		}
 	}
 	// return bottom right most element
-	return mpsGrid[m-1][n-1]
+	return grid[m-1][n-1]
 	
 }
