197708ac5824c7e14c675db417b9ecf130c8005c
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-04-11 16:11:38 -0700

    Adding improved solution
    Runtime
    29 ms
    Beats
    75.51%
    Memory
    7.4 MB
    Beats
    65.31%

--- leetcode/2390/main.go
+++ leetcode/2390/main.go
@@ -1,17 +1,26 @@
 func removeStars(s string) string {
 	starCount := 0
-	newStr := ""
+	lastIdxWritten := len(s)
 	for i:=len(s)-1; i >= 0; i--{
 		switch s[i] {
-        case '*':
-            starCount++
-        default:
-            if starCount > 0 {
-                starCount--
-            } else {
-                newStr = string(s[i]) + newStr
-            }
+			case '*':
+				starCount++
+			default:
+				if starCount > 0 {
+					starCount--
+					if starCount == 0 {
+						if lastIdxWritten == len(s) {
+							s = s[:i]
+						} else {
+							s = s[:i] + s[lastIdxWritten:]
+						}
+                        lastIdxWritten = i
+					}
+				} else {
+					lastIdxWritten = i
+                }
 		}
     }
-    return newStr
+    return s
+
 }
