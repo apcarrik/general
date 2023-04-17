0838a8a0e4337b8cf1a5acf55e8b3fad1437e665
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-04-17 15:27:31 -0700

    Solution with better memory usage
    Runtime
    34 ms
    Beats
    90.59%
    Memory
    13.8 MB
    Beats
    95.1%

--- leetcode/1431/main.py
+++ leetcode/1431/main.py
@@ -1,4 +1,6 @@
 class Solution:
 	def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
 		max_candies = max(candies)
-		return [c + extraCandies >= max_candies  for c in candies]
+		for i,c in enumerate(candies):
+			candies[i] = c + extraCandies >= max_candies
+		return candies
