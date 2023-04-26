42c86ef8005302541166b902e644dd19da90bcc2
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-04-26 16:04:29 -0700

    Updating with more elegant O(1) solution

--- leetcode/258/main.py
+++ leetcode/258/main.py
@@ -1,28 +1,8 @@
-""" Notes:
-    - 9 -> 9
-    - 99 -> 18 -> 9
-    - 999 -> 27 -> 9
-    - 9999 -> 36 -> 9
-    - 999999999999 -> 108 -> 9
-    - 9999999999999 -> 117 -> 9
-    - 8 -> 8
-    - 88 -> 16 -> 7
-    - 888 -> 24 -> 6
-    - 8888888 -> 56 -> 11 -> 2
-    - 2147483647 -> 46 -> 10 -> 1
-    - 999999999 -> 81 -> 9 
-
-"""
-
 class Solution:
-    def addDigits(self, num: int) -> int:
-        while num > 9:
-            digit_sum: int = 0
-            while num > 9:
-                digit_sum += num % 10
-                num //= 10
-            digit_sum += num % 10
-            num = digit_sum
-            
-        return num
-        
+    def addDigits(self, num: int) -> int:    
+        if num == 0: 
+            return 0
+        mod_nine: int = num % 9
+        if mod_nine == 0:
+            return 9      
+        return mod_nine
