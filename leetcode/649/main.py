d355cc38eee2cedc8847065ab8c9ec708201fb26
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-05-04 11:13:43 -0700

    Making code a little cleaner and more performant
    Runtime
    90 ms
    Beats
    33.33%
    Memory
    16.4 MB
    Beats
    15.60%

--- leetcode/649/main.py
+++ leetcode/649/main.py
@@ -2,22 +2,18 @@ class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         new_senate: string = ""
         r_count: int = 0
-        r_found: bool = False
         d_count: int = 0
-        d_found: bool = False
         for party in senate:
             if party == "R":
                 r_count += 1
                 d_count -= 1
                 if r_count > 0:
                     new_senate += "R"
-                    r_found= True
             else:
                 d_count += 1
                 r_count -= 1
                 if d_count > 0:
                     new_senate += "D"
-                    d_found = True
         # reconcile negative r or d count
         if r_count < 0:
             while r_count < 0:
@@ -27,10 +23,10 @@ class Solution:
             while d_count < 0:
                 new_senate = new_senate.replace("D","",1)
                 d_count += 1
-                
-        if r_found and not d_found:
+
+        if new_senate.count("R") > 0 and new_senate.count("D") == 0:
             return "Radiant"
-        elif d_found and not r_found:
+        elif new_senate.count("D") > 0 and new_senate.count("R") == 0:
             return "Dire"
 
         return self.predictPartyVictory(new_senate)
