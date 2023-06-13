d4edd6be667542ab554a1638eb7dad7a2a776523
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-06-13 16:12:51 -0700

    Adding a shorter solution using collections.Counter()
    Runtime
    128 ms
    Beats
    19.13%
    Memory
    21 MB
    Beats
    85.46%

--- leetcode/347/main.py
+++ leetcode/347/main.py
@@ -1,21 +1,3 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # create frequency map
-        fmap: dict[int,int] = {}
-        for num in nums:
-            if num in fmap:
-                fmap[num] += 1
-            else:
-                fmap[num] = 1
-
-        # find top k elements by min heap
-        topk: list[int] = []
-        heapq.heapify(topk)
-        for (elem, freq) in fmap.items():
-            if len(topk) < k:
-                heapq.heappush(topk, (freq,elem))
-            else:
-                if freq > topk[0][0]:
-                    heapq.heappop(topk)
-                    heapq.heappush(topk, (freq,elem))
-        return [elem for _,elem in topk]
+        return [k for k,_ in collections.Counter(nums).most_common(k)]
