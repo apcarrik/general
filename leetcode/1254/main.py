5803ae11e24a113234f502e3a87420bd67035528
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-06-16 09:40:56 -0700

    Replacing with a better solution
    Runtime
    134 ms
    Beats
    80.1%
    Memory
    16.7 MB
    Beats
    81.8%
    
    Inspired by [codermal7's solution](https://leetcode.com/problems/number-of-closed-islands/solutions/3385423/fastest-c-python-java-solution-runtime-3-ms-faster-than-100/?envType=study-plan-v2&envId=google-spring-23-high-frequency)

--- leetcode/1254/main.py
+++ leetcode/1254/main.py
@@ -1,20 +1,21 @@
 # class Solution:
 #     def closedIsland(self, grid: List[List[int]]) -> int:
 class Solution:
-    def dfs(self, x:int, y:int):
-        # turn this cell to water
-        self.grid[x][y] = 1
-        self.unchecked.discard((x,y))
+    def dfs(self, x:int, y:int) -> bool:
+        # Return true only if this and all connected land doesn't touch border            
+        if x < 0 or y < 0 or x >= self.n or y >= self.m:
+            return False
 
-        # check all 4 cells surrounding this (if they exist)
-        if x > 0 and self.grid[x-1][y] == 0:
-            self.dfs(x-1, y)
-        if y < self.m-1 and self.grid[x][y+1] == 0:
-            self.dfs(x,y+1)
-        if x < self.n-1 and self.grid[x+1][y] == 0:
-            self.dfs(x+1, y)
-        if y > 0 and self.grid[x][y-1] == 0:
-            self.dfs(x,y-1)
+        if self.grid[x][y] == 1:
+            return True
+
+        self.grid[x][y] = 1
+        top: bool = self.dfs(x-1,y)
+        right: bool = self.dfs(x,y+1)
+        down: bool = self.dfs(x+1,y)
+        left: bool = self.dfs(x,y-1)
+
+        return top and right and down and left
 
     def closedIsland(self, grid: list[list[int]]) -> int:
         self.grid = grid
@@ -23,24 +24,10 @@ class Solution:
             return 0
         self.m: int = len(self.grid[0])
 
-        # create set of all unchecked cells
-        self.unchecked: set[tuple[int,int]] = {(i,j) for i in range(self.n) for j in range(self.m)}
-
-        # check border to remove all non-closed islands
-        for x,y in [(0,j) for j in range(0,self.m)]\
-            + [(i,self.m-1) for i in range(1,self.n)]\
-            + [(self.n-1,j) for j in range(self.m-2,-1,-1)]\
-            + [(i, 0) for i in range(self.n-2, 0, -1)]:
-            if self.grid[x][y] == 0:
-                self.dfs(x,y)
-            self.unchecked.discard((x,y))
-
-        # go through remaining unchecked cells and perform dfs to find all islands
+        # go through all cells, calling dfs if they are land, and incrementing count if dfs returns true, indicating no boundaries encountered
         count: int = 0
-        while len(self.unchecked) > 0:
-            x,y = self.unchecked.pop()
-            if self.grid[x][y] == 0:
-                count += 1
-                self.dfs(x,y)
-            self.unchecked.discard((x,y))
+        for x in range(self.n):
+            for y in range(self.m):            
+                if self.grid[x][y] == 0 and self.dfs(x,y):
+                    count += 1
         return count
