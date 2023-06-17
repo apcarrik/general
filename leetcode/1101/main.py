6af82be49e9f33b5dcfd5a8c1c37fb77d9fa3711
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-06-17 16:30:05 -0700

    Just cleaning up code for readability

--- leetcode/1101/main.py
+++ leetcode/1101/main.py
@@ -1,20 +1,19 @@
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
-        idlookup: dict[int, int] = {i:i for i in range(n)} # nodeid:componentid
-        componentlookup: dict[int,set[int]] = {i:set([i]) for i in range(n)} #componentid:set(nodeids)
-        logs.sort() # assume logs is in chronological order, otherwise call logs.sort()
+        nlookup: dict[int, int] = {i:i for i in range(n)} # nodeid:componentid
+        clookup: dict[int,set[int]] = {i:set([i]) for i in range(n)} #componentid:set(nodeids)
+        logs.sort()
 
-        for l in logs:
-            ts, xi, yi = l
-            if idlookup[xi] != idlookup[yi]:
-                newcomponent: int = idlookup[xi]
-                oldcomponent: int = idlookup[yi]
-                if len(componentlookup[idlookup[xi]]) < len(componentlookup[idlookup[yi]]): # swapping to have oldcomponent be smaller component
-                    newcomponent, oldcomponent = oldcomponent, newcomponent
-                for nodeid in componentlookup[oldcomponent]:
-                    idlookup[nodeid] = newcomponent
-                componentlookup[newcomponent] |= componentlookup[oldcomponent]
-                del[componentlookup[oldcomponent]]
-                if len(componentlookup) == 1:
+        for ts, xi, yi in logs:
+            if nlookup[xi] != nlookup[yi]:
+                new: int = nlookup[xi]
+                old: int = nlookup[yi]
+                if len(clookup[nlookup[xi]]) < len(clookup[nlookup[yi]]): # swapping to have old be smaller component
+                    new, old = old, new
+                for nodeid in clookup[old]:
+                    nlookup[nodeid] = new
+                clookup[new] |= clookup[old]
+                del[clookup[old]]
+                if len(clookup) == 1:
                     return ts
         return -1
