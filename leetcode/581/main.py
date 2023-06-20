af8ad62ccaef79a154ccb9e3370de851f8ac094c
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-06-19 22:29:38 -0700

    Updating with O(n) runtime solution
    Ironically, has worse real runtime performance:
    
    Runtime
    228 ms
    Beats
    43.45%
    Memory
    18 MB
    Beats
    8.56%

--- leetcode/581/main.py
+++ leetcode/581/main.py
@@ -1,19 +1,26 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        nums2: list[int] = sorted(nums)
         n: int = len(nums)
-
+        if n <= 1:
+            return 0
+            
+        min_seen: list[int] = [nums[-1]]
+        for i in range(n-2,-1,-1):
+            min_seen.append(min(min_seen[-1],nums[i]))
         i1: int = -1
         for i in range(n):
-            if nums[i] != nums2[i]:
+            if nums[i] != min_seen[n-1-i]:
                 i1 = i
                 break
         if i1 == -1:
             return 0
 
+        max_seen: list[int] = [nums[0]]
+        for i in range(1,n):
+            max_seen.append(max(max_seen[-1],nums[i]))
         i2: int = -1
         for i in range(n-1,-1,-1):
-            if nums[i] != nums2[i]:
+            if nums[i] != max_seen[i]:
                 i2 = i
                 break
         return i2-i1+1
