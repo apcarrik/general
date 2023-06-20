fb7506d30c88a111ab433966c86980668bf393e1
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-06-20 11:12:43 -0700

    A bit cleaner code using bisect.bisect_left
    Runtime
    59 ms
    Beats
    43.23%
    Memory
    16.9 MB
    Beats
    5.60%

--- leetcode/33/main.py
+++ leetcode/33/main.py
@@ -1,6 +1,4 @@
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-
+from bisect import bisect_left
 class Solution:
     def search(self, nums: list[int], tgt: int) -> int:
         if len(nums) == 0:
@@ -16,10 +14,6 @@ class Solution:
             lo: int = 1
             hi: int = len(a)-1
             lastval: int = a[-1]
-            if a[lo] < a[lo-1]:
-                return lo
-            if a[hi] < a[hi-1]:
-                return hi
             while hi > lo:
                 mid: int = (hi-lo)//2 + lo
                 if a[mid] < a[mid-1]:
@@ -29,12 +23,11 @@ class Solution:
                     lo = mid + 1
                 else:
                     hi = mid - 1
-            
+
             if a[lo] < a[lo-1]:
                 return lo
             return -1
-
-        #  set r_idx to the index of the first element in the 2nd subarray        
+        #  set r_idx to the index of the first element in the 2nd subarray
         r_idx: int = decreaseBinsearch(nums)
 
         # 2. search for tgt in nums
@@ -49,24 +42,6 @@ class Solution:
             else:
                 max = r_idx-1
         # perform binsearch on nums[min:max+1]
-        def binsearch(a: list[int], lo: int, hi: int, tgt: int) -> int:
-            # searches for tgt in a within bounds a[lo:hi+1]. Returns -1 if not found
-            if a[lo] == tgt:
-                return lo
-            if a[hi] == tgt:
-                return hi
-            while hi > lo:
-                mid: int = (hi - lo)//2 + lo
-                if a[mid] == tgt:
-                    return mid
-                elif a[mid] > tgt:
-                    hi = mid - 1
-                else:
-                    lo = mid + 1
-            if a[lo] == tgt:
-                return lo
-            if lo > 0 and a[lo-1] == tgt:
-                return lo-1
-            return -1
-
-        return binsearch(nums, min, max, tgt)
+        bl: int = bisect_left(a=nums, x=tgt, lo=min, hi=max)
+        print(bl, n, nums[bl] if bl < n else "-",tgt)
+        return bl if nums[bl] == tgt else -1
